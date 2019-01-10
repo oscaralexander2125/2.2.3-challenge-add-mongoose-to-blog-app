241d8f36d61f63973bf82415e8cef6015e3062ae
Oscar A. Reyes <oscarreyes2194@gmail.com>
2019-01-10 17:11:23 -0600

    unfinished; response is empty

--- models.js
+++ models.js
@@ -1,22 +1,29 @@
 'use strict';
 
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const seedSchema = mongoose.Schema({
   title: {type: String, required: true},
-  author: {type: String, required: true},
-  content: {type: String, required: true}
+  author: {
+    firstName: String,
+    lastname: String,
+  },
+  content: {type: String, required: true},
+  created: {type: Date, default: Date.now}
 });
 
 seedSchema.virtual('authorName').get(function() {
-  return this.author.firstName + ' ' + this.author.lastName;
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 seedSchema.methods.serialize = function() {
   return {
+    id: this._id,
     title: this.title,
     content: this.content,
-    author: this.authorName
+    author: this.authorName,
+    created:this.created
   };
 };
 
